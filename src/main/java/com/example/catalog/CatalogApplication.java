0a0bf7269181c288cb25de9ea9028000e2b02982
anilmavis <77068958+anilmavis@users.noreply.github.com>
2022-04-17 15:43:19 +0300

    + made changes to Type and Types to be able to get existing status of types by using exceptions + added setter for name attribute in Type + added final keywords + done renaming for convenience. for example, addType(Type type) can be represented as add(Type type) since it is clear that Types class contains Type objects + done event binding of addTypeButton and exitButton + added javadocs + added menu bar

--- src/main/java/com/example/catalog/CatalogApplication.java
+++ src/main/java/com/example/catalog/CatalogApplication.java
@@ -6,27 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class CatalogApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("catalog-view.fxml")));
+    public void start(Stage stage) throws IOException {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("catalog-view.fxml")));
         stage.setTitle("catalog");
-        stage.setScene(new Scene(root,600,400));
+        stage.setScene(new Scene(parent, 600, 400));
         stage.show();
     }
 
     public static void main(String[] args) {
-        //launch(args);
-
-        Types types = new Types();
-
-        Type type = new Type("Book");
-        types.addType(type);
-        types.getType("Book");
-
-
-
+        launch(args);
     }
 }
---
+++ src/main/java/com/example/catalog/exception/field/FieldNotExistException.java
@@ -0,0 +1,10 @@
+package com.example.catalog.exception.field;
+
+import com.example.catalog.exception.CatalogException;
+
+public class FieldNotExistException extends CatalogException {
+
+    public FieldNotExistException() {
+        super("field does not exist");
+    }
+}
