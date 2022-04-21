7fc98dc38d6a1733bdc547f975f6a95bf3ec5271
Atabeydem <85640435+Atabeydem@users.noreply.github.com>
2022-04-21 22:45:43 +0300

    Here we go

--- src/main/java/com/example/catalog/Item.java
+++ src/main/java/com/example/catalog/Item.java
@@ -1,4 +1,83 @@
 package com.example.catalog;
 
+import com.example.catalog.exception.item.ItemExistException;
+import com.example.catalog.exception.item.ItemNotExistException;
+import java.util.List;
+import java.util.Set;
+
 public class Item {
+
+    public String name;
+    public Type type;
+    private Set <String> tags; //setter getter
+    private List<String> fieldValues;
+
+
+    public  Item(String name, List<String> fieldValues){
+        this.name=name;
+        this.fieldValues =fieldValues;
+    }
+
+
+    public void editField(String name, String newName) throws ItemNotExistException,ItemExistException{
+        int index = fieldValues.indexOf(name);
+
+        if(index ==-1){
+            throw new ItemNotExistException();
+        }
+        if(fieldValues.contains(newName)){
+            throw new ItemExistException();
+        }
+        fieldValues.set(index,newName);
+    }
+
+    public void addTag(String tag) throws ItemExistException{
+
+        if(tags.contains(tags)) {
+            throw new ItemExistException();
+        }
+
+        tags.add(tag);
+    }
+
+    public void removeTag(String tag) throws ItemNotExistException{
+
+        if(!tags.contains(tag)){
+            throw new ItemNotExistException();
+        }
+        tags.remove(tag);
+    }
+
+
+
+
+
+
+
+    public void editName(String newName){
+        setName(newName);
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Set < String> getTags() {return tags; }
+
+    public void setTags(Set <String> tags){this.tags=tags;}
+
+    public List<String> getFieldValues() {
+        return fieldValues;
+    }
+
+    public void setFieldValues(List<String> fieldValues) {
+        this.fieldValues = fieldValues;
+    }
+
+
+
 }
---
+++ src/main/java/com/example/catalog/exception/item/ItemExistException.java
@@ -0,0 +1,10 @@
+package com.example.catalog.exception.item;
+
+import com.example.catalog.exception.CatalogException;
+
+public class ItemExistException extends CatalogException {
+
+    public ItemExistException() {
+        super("item exists");
+    }
+}
