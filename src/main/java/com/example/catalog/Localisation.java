9a482a8fef47e0e826f1ee0bfee7d25ac6bbee1b
anilmavis <77068958+anilmavis@users.noreply.github.com>
2022-04-25 16:07:53 +0000

    Update Localisation.java

--- src/main/java/com/example/catalog/Localisation.java
+++ src/main/java/com/example/catalog/Localisation.java
@@ -1,7 +1,12 @@
 package com.example.catalog;
 
 public class Localisation {
-    public static final String HELP = "Help";
+    public static final String HELP_TITLE = "Help";
+    public static final String HELP_TEXT = """
+    1. Select a type in the view and click the add button to add an item
+    2. Select a type or an item and click the remove or edit button
+    3. Select an item and click the add tag button
+    """;
     public static final String SELECT = "Select";
     public static final String ADD = "Add";
     public static final String ROOT = "Root";
@@ -11,6 +16,5 @@ public class Localisation {
     public static final String BLANK_FIELD = "Field is blank";
     public static final String SELECTED_ROOT = "Root is selected";
     public static final String SELECTED_TYPE = "Type is selected";
-
     public static final String BLANK_TAG = "Tag is blank";
 }
